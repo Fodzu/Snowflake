8047ea232a64f0062812495e4f2bcd68b1a71c7c
Fodzu <149393214+Fodzu@users.noreply.github.com>
2024-01-22 23:25:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var links = document.querySelectorAll('.navbar a');
-    var body = document.body;
+  const swup = new Swup(); // Initialize Swup
+  swup.options = {
+      ...swup.options,
+      plugins: [
+          new SwupScrollPlugin(),
+          new SwupPreloadPlugin(),
+          new SwupDebugPlugin(),
+          new SwupGAPlugin(),
+      ],
+  };
 
-    links.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            
-            // Add the 'transition-fade' class to initiate the fade-out transition
-            body.classList.add('transition-fade');
-            
-            // Get the target page URL
-            var targetPage = link.getAttribute('href');
-
-            // Wait for the transition to complete before navigating to the new page
-            setTimeout(function () {
-                window.location.href = targetPage;
-            }, 500); // Adjust the timeout to match your transition duration
-        });
-    });
-
-    // Add an event listener to remove the 'transition-fade' class on page load
-    window.addEventListener('pageshow', function (event) {
-        if (event.persisted) {
-            // Remove the 'transition-fade' class to initiate the fade-in transition
-            body.classList.remove('transition-fade');
-        }
-    });
 });
