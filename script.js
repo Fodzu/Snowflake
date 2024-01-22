eb25e2a5f234f978c6b83464e876b0ac535c6dc4
Fodzu <149393214+Fodzu@users.noreply.github.com>
2024-01-22 20:19:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a').forEach(function (link) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.body.classList.add('transition-fade');
+    var links = document.querySelectorAll('.navbar a');
+    var body = document.body;
 
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            
+            // Add the 'transition-fade' class to initiate the fade-out transition
+            body.classList.add('transition-fade');
+            
+            // Get the target page URL
+            var targetPage = link.getAttribute('href');
+
+            // Wait for the transition to complete before navigating to the new page
             setTimeout(function () {
-                window.location.href = link.href;
-            }, 500);
+                window.location.href = targetPage;
+            }, 500); // Adjust the timeout to match your transition duration
         });
+    });
+
+    // Add an event listener to remove the 'transition-fade' class on page load
+    window.addEventListener('pageshow', function (event) {
+        if (event.persisted) {
+            // Remove the 'transition-fade' class to initiate the fade-in transition
+            body.classList.remove('transition-fade');
+        }
     });
 });
