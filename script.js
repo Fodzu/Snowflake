e91bb29bbb4161d3fc0a13fa1193a0e986496c8c
Fodzu <149393214+Fodzu@users.noreply.github.com>
2024-01-23 00:10:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var links = document.querySelectorAll('.navbar a');
-  var body = document.body;
+    var body = document.body;
 
-  links.forEach(function (link) {
-      link.addEventListener('click', function (event) {
-          event.preventDefault();
+    // Remove the 'transition-fade' class to initiate the fade-in transition
+    body.classList.remove('transition-fade');
 
-          // Add the 'transition-fade' class to initiate the fade-out transition
-          body.classList.add('transition-fade');
+    // Adding the fade-in class after a small delay to allow fade-out to complete
+    setTimeout(function () {
+        body.classList.add('fade-in');
+    }, 500); // Adjust the delay time as needed (500ms in this example)
 
-          // Get the target page URL
-          var targetPage = link.getAttribute('href');
+    var links = document.querySelectorAll('.navbar a');
 
-          // Wait for the transition to complete before navigating to the new page
-          setTimeout(function () {
-              window.location.href = targetPage;
-          }, 500); // Adjust the timeout to match your transition duration
-      });
-  });
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
 
-  // Add an event listener to remove the 'transition-fade' class on page load
-  window.addEventListener('pageshow', function (event) {
-      if (event.persisted) {
-          // Remove the 'transition-fade' class to initiate the fade-in transition
-          body.classList.remove('transition-fade');
-      }
-  });
+            // Add the 'transition-fade' class to initiate the fade-out transition
+            body.classList.add('transition-fade');
+
+            // Get the target page URL
+            var targetPage = link.getAttribute('href');
+
+            // Wait for the transition to complete before navigating to the new page
+            setTimeout(function () {
+                window.location.href = targetPage;
+            }, 500); // Adjust the timeout to match your transition duration
+        });
+    });
+
+    // Add an event listener to remove the 'transition-fade' class on page load
+    window.addEventListener('pageshow', function (event) {
+        if (event.persisted) {
+            // Remove the 'transition-fade' class to initiate the fade-in transition
+            body.classList.remove('transition-fade');
+        }
+    });
 });
